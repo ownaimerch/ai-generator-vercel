0cbc3cf3659d89ef8d765694454d81caa57e7032
ownaimerch <orginalaimerch@gmail.com>
2025-08-23 16:28:49 +0200

    Delete api/ok.ts

--- api/ok.ts
+++
@@ -1,4 +0,0 @@
-import type { VercelRequest, VercelResponse } from 'vercel';
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({ ok: true, host: req.headers.host, time: new Date().toISOString() });
-}
