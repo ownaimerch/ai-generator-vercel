ec97a5dd22c2929f78563d56c9fc8d53f568c484
ownaimerch <orginalaimerch@gmail.com>
2025-11-17 10:18:53 +0100

    Delete api/debug/has-printify-app-id.js

--- api/debug/has-printify-app-id.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    hasAppId: Boolean(process.env.PRINTIFY_APP_ID),
-    presentKeys: Object.keys(process.env).filter(k => k.startsWith('PRINTIFY'))
-  });
-}
