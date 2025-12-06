2a9a98954258f8c0660dd9efd74d187c4d8aca5f
ownaimerch <orginalaimerch@gmail.com>
2025-12-06 12:03:40 +0100

    Delete api/ok.js

--- api/ok.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    host: req.headers.host,
-    time: new Date().toISOString(),
-    pid: typeof process !== 'undefined' ? process.pid : null
-  });
-}
